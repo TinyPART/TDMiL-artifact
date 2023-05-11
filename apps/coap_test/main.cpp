087f8349763e74d3fe76612c7b3ea3e5b97244d9
Mak <mayank.gulati@fu-berlin.de>
2023-05-11 14:02:32 +0200

    sending params in return of train func

--- apps/coap_test/main.cpp
+++ apps/coap_test/main.cpp
@@ -10,10 +10,10 @@
 
 #include "coap_ml.h"
 
-#define MAIN_QUEUE_SIZE     (8U)
+#define MAIN_QUEUE_SIZE (8U)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
-static float _ml_values[CONFIG_COAP_ML_NUM_PARAMS] = { 0 };
+static float _ml_values[CONFIG_COAP_ML_NUM_PARAMS] = {0};
 static mutex_t values_lock = MUTEX_INIT;
 
 static void _write_ml_values(void *ctx, float values[CONFIG_COAP_ML_NUM_PARAMS])
@@ -42,15 +42,17 @@ int main()
 
     coap_ml_server_init(_read_ml_values, _write_ml_values, NULL);
 
-    std::vector<float> ms{ 3.5, 9.3, 5 };
+    std::vector<float> ms{3.5, 9.3, 5};
     float b = 1.7;
     puts("Training set:\n");
     SyntheticDataset local_training_data = SyntheticDataset(ms, b, 1000);
     SyntheticDataset local_testing_data = SyntheticDataset(ms, b, 100);
     LineFitModel local_model = LineFitModel(1000, 0.001, 3);
-    std::tuple<size_t, float, float> output_train = local_model.train_SGD(local_training_data);
-    printf("Training Loss: %f\n", std::get<1>(output_train));
-    printf("Training Accuracy: %f\n", std::get<2>(output_train));
+    std::tuple<size_t, float, float, float, float> output_train = local_model.train_SGD(local_training_data);
+    printf("Training param[0]: %f\n", std::get<1>(output_train));
+    printf("Training param[1]: %f\n", std::get<2>(output_train));
+    printf("Training param[2]: %f\n", std::get<3>(output_train));
+    printf("Training param[3]: %f\n", std::get<4>(output_train));
 
     std::tuple<size_t, float, float> output_test = local_model.evaluate(local_testing_data);
     printf("testing Loss: %f\n", std::get<1>(output_test));
