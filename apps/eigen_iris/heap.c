8c454d33260e9e485fdd40267553b808e7f8c469
Koen Zandberg <koen@bergzand.net>
2024-01-26 14:04:03 +0100

    Add test application with Iris data

---
+++ apps/eigen_iris/heap.c
@@ -0,0 +1,4 @@
+void print_heap_stats(void) {
+    extern void heap_stats(void);
+    heap_stats();
+}
---
+++ apps/eigen_iris/heap.h
@@ -0,0 +1,8 @@
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void print_heap_stats(void);
+#ifdef __cplusplus
+}
+#endif
