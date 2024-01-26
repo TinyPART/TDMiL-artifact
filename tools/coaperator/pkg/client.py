a8817d12dcdf45db66395df73116702141fed6aa
Koen Zandberg <koen@bergzand.net>
2024-01-26 14:01:43 +0100

    CoAPerator: Add control endpoint for aggregator

--- tools/coaperator/pkg/client.py
+++ tools/coaperator/pkg/client.py
@@ -1,4 +1,5 @@
 import asyncio
+import enum
 import logging
 
 from pkg.corerd import Registration
@@ -6,7 +7,14 @@ from pkg.channel import CoapChannel
 from pkg.mlcontrol import MLController
 
 
+
+
 class Client(Registration):
+
+    class State(enum.Enum):
+        REGISTERED = 1
+        ESTABLISHED = 2
+
     def __init__(
         self,
         static_registration_parameters,
@@ -33,13 +41,19 @@ class Client(Registration):
         name = "/".join(static_registration_parameters["ep"])
         self.logger = logging.getLogger(f"device.{name}")
         self.logger.info(f"New device at {network_remote.sockaddr[0]}")
-        self.channel = CoapChannel(network_remote, "/ml", context)
+        self.channel = CoapChannel(network_remote, "/ev", context)
         self.mlcontrol = MLController(self.channel)
         asyncio.create_task(self._start_channel())
         asyncio.create_task(self._maintain_status())
 
+    def _channel_data(self, *args):
+        self.logger.info(f"error observe: {args}")
+
+    def _channel_err(self, *args):
+        self.logger.error(f"error observe: {args}")
+
     async def _start_channel(self):
-        await self.channel.start_observe()
+        await self.channel.start_observe(self._channel_data, self._channel_err)
 
     async def push_data(self, payload):
         await self.channel.submit(payload)
